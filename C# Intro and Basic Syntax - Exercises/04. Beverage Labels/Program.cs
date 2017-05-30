a08cb7ee6c5809d50fe9c5d87b74f835c6398be5
DJColdBrain <djcoldbrain@gmail.com>
2017-05-30 19:04:42 +0300

    Changed more names and paths

---
+++ C# Intro and Basic Syntax - Exercises/04. Beverage Labels/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + "ml " + s + ":");
+            Console.WriteLine("{0}kcal, {1}g sugars", b * a / 100, c * a / 100);
+
+
+        }
+    }
+}
