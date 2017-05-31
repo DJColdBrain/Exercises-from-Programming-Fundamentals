c851e88ed7df8be34d53120d38ff778ac6f49d56
DJColdBrain <djcoldbrain@gmail.com>
2017-05-31 16:05:35 +0300

    Added Lab exersises/ 4 more

---
+++ Data Types and Variables/04. Elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+                 int e = int.Parse (Console .ReadLine());
+            float c = float.Parse(Console.ReadLine());
+            int cources = (int)Math.Ceiling((double)(e / c));
+
+            Console.WriteLine(cources);
+
+        }
+    }
+}
