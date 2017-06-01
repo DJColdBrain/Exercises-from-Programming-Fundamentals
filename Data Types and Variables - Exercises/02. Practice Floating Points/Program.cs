b23ddf466766eef298c317a7f63b3d45ec0f0789
DJColdBrain <djcoldbrain@gmail.com>
2017-06-01 14:22:27 +0300

    Adding exercises

---
+++ Data Types and Variables - Exercises/02. Practice Floating Points/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Practice_Floating_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
